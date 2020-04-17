b38bdc386cc8d1f2d705e912c31d27c633c37f44
Matias Seguel <mseguelestay@gmail.com>
2020-04-16 21:37:15 -0400

    Added Nickname to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :jams
   get 'home/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # get 'players/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
